18956f2862260ed24e58232159d83efc840f8ac8
regularbagel <scottdlawrence@gmail.com>
2016-03-10 17:56:34 -0600

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1,1 +1,16 @@
-
+var Board = function (columns, rows) {
+  
+    var board = [];
+    var group = game.add.group();
+    
+    for (var y=0; y<rows; y++) {
+        var row = [];
+ 
+        for (var x=0; x<columns; x++) {
+            var tile = new Tile(x, y, group);
+            row.push(tile);
+        }
+        
+        board.push(row);
+    }
+}
