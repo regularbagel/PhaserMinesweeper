bd4cb91f1883e9422fc6a484b45e4a1e4c69d12c
regularbagel <scottdlawrence@gmail.com>
2016-03-10 17:45:37 -0600

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,20 +1,40 @@
 var gameProperties = {
     screenWidth: 640,
     screenHeight: 480,
+    
+    tileWidth: 32,
+    tileHeight: 32,
+        
+    boardWidth: 9,
+    boardHeight: 9,
+    
+    totalMines: 10,
 };
 
 var states = {
     game: "game",
 };
 
+var graphicAssets = {
+    tiles:{URL:'assets/tiles.png', name:'tiles', frames:14},
+};
+
 var gameState = function(game){
-    
+    this.boardTop;
+    this.boardLeft;
+    this.board;
 };
 
 gameState.prototype = {
     
+    init: function () {
+         this.boardTop = (gameProperties.screenHeight - (gameProperties.tileHeight * gameProperties.boardHeight)) * 0.5;
+         this.boardLeft = (gameProperties.screenWidth - (gameProperties.tileWidth * gameProperties.boardWidth)) * 0.5;
+  
+    }
+    
     preload: function () {
-        
+        game.load.spritesheet(graphicAssets.tiles.name, graphicAssets.tiles.URL, gameProperties.tileWidth, gameProperties.tileHeight, graphicAssets.tiles.frames);
     },
     
     create: function () {
