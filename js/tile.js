7af3e188383fd48db7a21c8d706348732b53b5ad
regularbagel <scottdlawrence@gmail.com>
2016-03-11 14:49:01 -0600

    Added files via upload

--- js/tile.js
+++ js/tile.js
@@ -1,6 +1,6 @@
 var Tile = function (column, row, group) {
-  
-  var states = {
+    
+    var states = {
         ZERO: 0,
         ONE: 1,
         TWO: 2,
@@ -15,45 +15,46 @@ var Tile = function (column, row, group) {
         WRONG_FLAG: 11,
         UNKNOWN: 12,
         MINE: 13,
-  };
-  
-  this.column = column;
-  this.row = row;
-  this.x = column * gameProperties.tileWidth;
-  this.y = row * gameProperties.tileHeight;
-  
-  var tile = this;
-  var currentFrame = states.DEFAULT;
-  var currentState = states.ZERO;
-  var sprite = game.add.sprite(column * gameProperties.tileWidth, row * gameProperties.tileHeight, graphicAssets.tiles.name, currentFrame, group);
-
-  var init = function () {
-      sprite.inputEnabled = true;
-      sprite.input.useHandCursor = true;
-      sprite.events.onInputOut.add(rollOut, this);
-      sprite.events.onInputOver.add(rollOver, this);
-      sprite.events.onInputDown.add(click, this);
-  };
-  
-  var rollOver = function () {
-      var tween = game.add.tween(sprite);
-      tween.to({x:tile.x-3, y:tile.y-3}, 100, Phaser.Easing.Exponential.easeOut);
-      tween.start();
-  };
-  
-  var rollOut = function () {
-      var tween = game.add.tween(sprite);
-      tween.to({x:tile.x, y:tile.y}, 100, Phaser.Easing.Exponential.easeOut);
-      tween.start();
-  }
-  
-  var click = function () {
-      tile.reveal();
-  }
-  
-  this.reveal = function () {
-      sprite.animations.frame = currentValue;
-      sprite.inputEnabled = false;
-  };
-  
-}
+    };
+    
+    this.column = column;
+    this.row = row;
+    this.x = column * gameProperties.tileWidth;
+    this.y = row * gameProperties.tileHeight;
+    
+    var tile = this;
+    var currentState = states.DEFAULT;
+    var currentValue = 0;
+    var sprite = game.add.sprite(this.x, this.y, graphicAssets.tiles.name, currentState, group);
+    
+    var init = function () {        
+        sprite.inputEnabled = true;
+        sprite.input.useHandCursor = true;
+        sprite.events.onInputOut.add(rollOut, this);
+        sprite.events.onInputOver.add(rollOver, this);
+        sprite.events.onInputDown.add(click, this);
+    };
+    
+    var rollOver = function () {
+        var tween = game.add.tween(sprite);
+        tween.to({x:tile.x-3, y:tile.y-3}, 100, Phaser.Easing.Exponential.easeOut);
+        tween.start();
+    };
+    
+    var rollOut = function () {
+        var tween = game.add.tween(sprite);
+        tween.to({x:tile.x, y:tile.y}, 100, Phaser.Easing.Exponential.easeOut);
+        tween.start();
+    }
+    
+    var click = function () {
+        tile.reveal();
+    }
+    
+    this.reveal = function () {
+        sprite.animations.frame = currentValue;
+        sprite.inputEnabled = false;
+    };
+    
+    init();
+};
